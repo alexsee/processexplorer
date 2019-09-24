8abd4d74c3bba3b671ebf9e105fe7bedb5ee40dc
Alexander Seeliger <alexander.seeliger@alexosoft.de>
2019-09-24 18:18:11 +0200

    Add CORS exclude

--- webservice/src/main/java/de/tk/processmining/webservice/WebserviceApplication.java
+++ webservice/src/main/java/de/tk/processmining/webservice/WebserviceApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import de.tk.processmining.data.storage.StorageProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -12,6 +15,18 @@ public class WebserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebserviceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 
 }
