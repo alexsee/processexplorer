c13347978c87c14da7cd42adf1203655d0b5d975
Alexander Seeliger <alexander.seeliger@alexosoft.de>
2019-10-18 10:11:53 +0200

    Fix production settings

--- webportal/src/environments/environment.prod.ts
+++ webportal/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serviceUrl: 'http://localhost:8080'
+  serviceUrl: 'http://localhost:8080',
+  webSocketEndPoint: 'ws://localhost:8080/socket'
 };
