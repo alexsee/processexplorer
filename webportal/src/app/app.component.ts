278209fd6c58100a6fa20e7c475b2eebf7ad053e
Alexander Seeliger <alexander.seeliger@alexosoft.de>
2019-12-13 15:15:51 +0100

    Fix small implement issue

--- webportal/src/app/app.component.ts
+++ webportal/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { LogService } from './log/shared/log.service';
 import { EventLog } from './log/models/eventlog.model';
 
@@ -7,7 +7,7 @@ import { EventLog } from './log/models/eventlog.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'webportal';
 
   logs: EventLog[];
