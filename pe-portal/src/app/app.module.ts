308beef763eb681e7777af5bbf5cc0bb6816254b
Alexander Seeliger <alexander.seeliger@alexosoft.de>
2020-09-30 11:44:47 +0200

    Update module dependencies

--- pe-portal/src/app/app.module.ts
+++ pe-portal/src/app/app.module.ts
@@ -9,7 +9,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnalysisComponent } from './modules/process-mining/analysis/analysis.component';
 
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
 
 import { HighchartsChartModule } from 'highcharts-angular';
 import { GridsterModule } from 'angular-gridster2';
@@ -38,6 +43,29 @@ import { DurationConditionComponent } from './analysis/conditions/duration-condi
 import { ResourcePathConditionComponent } from './analysis/conditions/resource-path-condition/resource-path-condition.component';
 import { DashboardComponent } from './modules/process-mining/dashboard/dashboard.component';
 import { ChartComponent } from './analysis/components/chart/chart.component';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzMessageModule } from 'ng-zorro-antd/message';
 
 @NgModule({
   declarations: [
@@ -76,7 +104,34 @@ import { ChartComponent } from './analysis/components/chart/chart.component';
     AppRoutingModule,
     BrowserAnimationsModule,
 
-    NgZorroAntdModule,
+    NzButtonModule,
+    NzTableModule,
+    NzInputModule,
+    NzSelectModule,
+    NzDividerModule,
+    NzDropDownModule,
+    NzSpinModule,
+    NzTagModule,
+    NzLayoutModule,
+    NzSliderModule,
+    NzBreadCrumbModule,
+    NzFormModule,
+    NzUploadModule,
+    NzPageHeaderModule,
+    NzListModule,
+    NzProgressModule,
+    NzAvatarModule,
+    NzStatisticModule,
+    NzCardModule,
+    NzDrawerModule,
+    NzCollapseModule,
+    NzModalModule,
+    NzPopoverModule,
+    NzRadioModule,
+    NzInputNumberModule,
+    NzEmptyModule,
+    NzMessageModule,
+
     HighchartsChartModule,
 
     FormsModule,
